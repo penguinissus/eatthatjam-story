7daef3b5ab83df51b1b39d8c9413fcdee2982e21
penguinissus <jocelyn.ty.lau@gmail.com>
2025-07-12 22:59:39 -0400

    jswing setup

--- gui.java
+++ gui.java
@@ -1,2 +1,42 @@
-import javax.swing.*;
-import java.awt.event.*;
+import javax.swing.*; //jswing
+import java.awt.*; //action listener
+import java.io.File; //file io
+import java.io.IOException; //more file io
+import javax.imageio.ImageIO; //jswing image stuff
+
+public class Gui extends JFrame implements ActionListener{
+    static JFrame jframe1;
+
+    public Gui(){
+        super("EATTHATJAM");
+        openFrame1();
+    }
+
+    public void openFrame1(){
+        //remove this if section if only 1 frame
+        if(frame1==null){
+            frame1 = new JFrame("EATTHATJAM");
+
+            int frameSizeMultiplier = 25;
+            int FRAME_WIDTH = 16*frameSizeMultiplier;
+            int FRAME_HEIGHT = 9*frameSizeMultiplier;
+
+            nextButton = new JButton("Next event");
+
+            nextButton.addActionListener(this);
+
+            frame1.setLayout(null);
+
+            //set bounds for components
+            //(x,y,width,height)
+            nextButton.setBounds(100,100,100,100);
+
+            //add components to frame
+            frame1.add(nextButton);
+
+            //frame settings
+            frame1.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+            frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
+    }
+}
